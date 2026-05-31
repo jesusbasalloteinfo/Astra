17967db83ae27584016efbfe76639765c80df1a4
Jesús Basallote Gallardo <jesusbasalloteinfo@gmail.com>
2026-06-01 00:12:21 +0200

    small change

--- astra_ui/src/routes/+layout.svelte
+++ astra_ui/src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	import { page } from '$app/state';
 	let { children } = $props();
@@ -23,5 +22,4 @@
     });
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
