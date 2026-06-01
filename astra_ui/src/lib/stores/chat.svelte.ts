95da4f2f7922730c9cae27c5285239e0d2aa094d
Jesús Basallote Gallardo <jesusbasalloteinfo@gmail.com>
2026-06-01 21:11:16 +0200

    python and typescript files license

--- astra_ui/src/lib/stores/chat.svelte.ts
+++ astra_ui/src/lib/stores/chat.svelte.ts
@@ -1,3 +1,21 @@
+/*
+ * ASTRA - Automated Smart Telescope Remote Assistant
+ * Copyright (C) 2026 Jesus Basallote
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { chatAPI } from '$lib/api/chat';
 import type { ChatSession, ChatMessage, ToolCall, ToolCallDefinition } from '$lib/types/chat';
 import { selectionStore } from '$lib/stores/activeSelection.svelte';
--- sideris/models/catalog/sidereal.py
+++ sideris/models/catalog/sidereal.py
@@ -1,3 +1,21 @@
+"""
+ASTRA - Automated Smart Telescope Remote Assistant
+Copyright (C) 2026 Jesus Basallote
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from pydantic import BaseModel, Field
 from typing import List, Optional, Dict, Any
 class Star(BaseModel):
